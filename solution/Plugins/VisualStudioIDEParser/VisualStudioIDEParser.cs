963b82105eba0ef43c9edeca8f5cae14c2a96828
udragan <ujfa2001@yahoo.com>
2017-10-24 13:47:57 +0200

    vs plugin - retrieve all project files.

--- solution/Plugins/VisualStudioIDEParser/VisualStudioIDEParser.cs
+++ solution/Plugins/VisualStudioIDEParser/VisualStudioIDEParser.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
 using com.udragan.csharp.Orphanator.Common.Interfaces;
 
 namespace com.udragan.csharp.Orphanator.Plugins.VisualStudioIDEParser
@@ -50,6 +52,23 @@ namespace com.udragan.csharp.Orphanator.Plugins.VisualStudioIDEParser
 		public IEnumerable<string> Handle(string[] args)
 		{
 			IEnumerable<string> result = new List<string>();
+
+			if (!Directory.Exists(args[3]))
+			{
+				Console.WriteLine(string.Format("Directory {0} does not exist!",
+					args[3]));
+
+				return result;
+			}
+
+			// retrieve all project files
+			//TODO: consider all vs project files, not only csproj
+			IEnumerable<string> projectFilePaths = Directory.EnumerateFiles(args[3], "*.csproj", SearchOption.AllDirectories);
+
+			Console.WriteLine("\nFound project files:");
+			Console.WriteLine("---------------------------");
+			Console.WriteLine(string.Join("\n", projectFilePaths.Select(x => Path.GetFullPath(x))));
+			Console.WriteLine();
 
 			return result;
 		}
